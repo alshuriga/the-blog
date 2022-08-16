61bee741cea96816102d98432ba552fc4af387fe
Oleksandr Shuryha <alshuriga@gmail.com>
2022-08-16 22:55:36 +0200

    minor refactioring

---
+++ src/MiniBlog.Core/Exceptions/MiniBlogException.cs
@@ -0,0 +1,10 @@
+public class MiniBlogWebException : ApplicationException
+{
+    public string ReturnUrl { get; private set; } = "/";
+    public MiniBlogWebException(string? message, string? returnUrl = null): base(message)
+    {
+        if(returnUrl != null) ReturnUrl = returnUrl;
+    }
+    
+
+}
